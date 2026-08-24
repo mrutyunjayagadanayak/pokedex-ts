c23ba797199c1b1d5415dfb98666640eedd81272
Bapun <mrutyunjayagadanayak@yandex.com>
2026-08-24 19:55:59 +0530

    Added repl

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,30 @@
+import readline from "readline";
 
 export function cleanInput(val: string): string[] {
   if (val.length === 0) {
     return [""];
   }
   return val.toLowerCase().trim().split(/[ ,]+/);
+}
+
+export function startREPL(): void {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output:process.stdout,
+    prompt:"Pokedex > "
+  });
+
+  rl.prompt();
+
+  rl.on("line", (input) => {
+    if (!input) {
+      rl.prompt();
+    }
+    let data = cleanInput(input);
+    if (data.length === 0) {
+      rl.prompt();
+    }
+    console.log(`Your command was: ${data[0]}`);
+    rl.prompt();
+  })
 }
